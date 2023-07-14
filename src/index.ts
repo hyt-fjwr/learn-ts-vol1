f9d167015f10bdc9bfdd56da1402b2769086689b
hyt-fjwr <f.hayato0104@gmail.com>
2023-07-14 11:47:45 +0900

    -1

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,19 @@ console.log(msg);
 const result: bigint = 5n / 2n;
 console.log(result);
 
+//tsではnullよりundefinedを推奨している
+const val1 = null;
+const val2 = undefined;
+console.log(val1, val2);
 
+//型変換
+//型変換には暗黙的な型変換(型推論)、明示的な型変換のに種類がある
+console.log(Number(true));
+console.log(Number(false));
+
+console.log(Number(undefined));
+
+
+//算術演算子
+const sqrt = 2 ** 5; //2の5乗
+console.log(sqrt);
